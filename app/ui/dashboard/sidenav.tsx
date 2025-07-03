0a995710811ab59f2cf46c3559e8f5bdabf6779e
Gbolahan <shololagbolahan@gmail.com>
2025-07-03 18:51:46 +0100

    9. Implementing Partial Prerendering

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,9 +1,11 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link"
+import NavLinks from "@/app/ui/dashboard/nav-links"
+import AcmeLogo from "@/app/ui/acme-logo"
+import { PowerIcon } from "@heroicons/react/24/outline"
 
 export default function SideNav() {
+  export const experimental_ppr = true
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -25,5 +27,5 @@ export default function SideNav() {
         </form>
       </div>
     </div>
-  );
+  )
 }
