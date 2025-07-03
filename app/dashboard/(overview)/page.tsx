d2741926919674214593144c3531b197b82d5039
Gbolahan <shololagbolahan@gmail.com>
2025-07-03 17:34:53 +0100

    8.1 Adding loading skeletons

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,18 +1,22 @@
-import { Card } from "@/app/ui/dashboard/cards"
-import RevenueChart from "@/app/ui/dashboard/revenue-chart"
-import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
-import { lusitana } from "@/app/ui/fonts"
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data"
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from "../../lib/data";
 
 export default async function Page() {
-  const revenue = await fetchRevenue()
-  const latestInvoices = await fetchLatestInvoices()
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
     numberOfCustomers,
     numberOfInvoices,
-  } = await fetchCardData()
+  } = await fetchCardData();
 
   return (
     <main>
@@ -34,5 +38,5 @@ export default async function Page() {
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
-  )
+  );
 }
