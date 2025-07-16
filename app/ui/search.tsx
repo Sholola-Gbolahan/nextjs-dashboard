1901bbf8df1de261ba0cf619c00df217a3f987b4
Gbolahan <shololagbolahan@gmail.com>
2025-07-16 07:11:04 +0100

    - 10.5 Adding Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,31 +1,32 @@
-"use client"
+"use client";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
-import { useSearchParams, usePathname, useRouter } from "next/navigation"
-import { useDebouncedCallback } from "use-debounce"
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   // useSearchParams- Allows you to access the parameters of the current URL. For example, the search params for this URL /dashboard/invoices?page=1&query=pending would look like this: {page: '1', query: 'pending'}.
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
   // usePathname- Returns the current pathname of the URL. For example, if you are on /dashboard/invoices, it will return /dashboard/invoices.
-  const pathname = usePathname()
+  const pathname = usePathname();
   // useRouter- Provides methods to navigate programmatically, such as replace, push, etc
-  const { replace } = useRouter()
+  const { replace } = useRouter();
 
   // This function will wrap the contents of handleSearch, and only run the code after a specific time once the user has stopped typing (300ms).
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`)
+    console.log(`Searching... ${term}`);
     // URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
-    const params = new URLSearchParams(searchParams)
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
 
     if (term) {
-      params.set(`query`, term)
+      params.set(`query`, term);
     } else {
-      params.delete(`query`)
+      params.delete(`query`);
     }
 
-    replace(`${pathname}?${params.toString()}`)
-  }, 300)
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -36,12 +37,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value)
+          handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get("query")?.toString()}
       />
 
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
-  )
+  );
 }
